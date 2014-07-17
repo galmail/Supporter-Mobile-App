80a08d206d65673748d2483357480b126b404965
Laura Carvajal <laura.carvajal@corp.badoo.com>
2014-07-17 19:55:42 +0100

    path fix

--- app/js/routers/router.js
+++ app/js/routers/router.js
@@ -5,28 +5,30 @@ define([
 	'foundation',
 	'text!templates/footer.html',
 	'text!templates/home.html'
-], function ($, Backbone, Foundation, homeTemplateSrc) {
+], function ($, Backbone, Foundation, footerTemplateSrc, homeTemplateSrc) {
 	'use strict';
 
 	var body 		= $('body');
-	var containr 	= $('.js-main-container');
+	var container 	= $('.js-main-container');
 	var footer 		= $('.js-footer');
 
 	var Router = Backbone.Router.extend({
 
 		routes: {
-			'home': 'home', //http://localhost/#home
-			'login': 'login' //http://localhost/#login
+			'home': 'home', 	//http://localhost/#home
+			'login': 'login' 	//http://localhost/#login
 		},
 
 		home: function (param) {
 			body.addClass('body-not-logged');
-			containr.html(_.template(homeTemplateSrc));
+			container.html(_.template(homeTemplateSrc));
 			footer.html(_.template(footerTemplateSrc));
 		},
 
 		login: function (param) {
-			alert('login');
+			body.addClass('body-not-logged');
+			//
+			footer.html(_.template(footerTemplateSrc));
 		}
 
 	});
