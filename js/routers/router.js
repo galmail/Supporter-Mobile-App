c204f593c61887208deb268c0a7f75257976a547
Laura Carvajal <laura.carvajal@corp.badoo.com>
2014-07-17 19:39:19 +0100

    footer

--- js/routers/router.js
+++ js/routers/router.js
@@ -3,30 +3,32 @@ define([
 	'jquery',
 	'backbone',
 	'foundation',
+	'text!templates/footer.html',
 	'text!templates/home.html'
-], function ($, Backbone, Foundation, templateSrc) {
+], function ($, Backbone, Foundation, homeTemplateSrc) {
 	'use strict';
 
-	var mainContainer = $('.js-main-container');
+	var body 		= $('body');
+	var containr 	= $('.js-main-container');
+	var footer 		= $('.js-footer');
 
 	var Router = Backbone.Router.extend({
+
 		routes: {
 			'home': 'home', //http://localhost/#home
 			'login': 'login' //http://localhost/#login
 		},
+
 		home: function (param) {
-			console.info('src', templateSrc);
-			var data = {
-				name: 'moe'
-			};
-
-			var compiled = _.template(templateSrc, data);
-
-			mainContainer.html(compiled);
+			body.addClass('body-not-logged');
+			containr.html(_.template(homeTemplateSrc));
+			footer.html(_.template(footerTemplateSrc));
 		},
+
 		login: function (param) {
 			alert('login');
 		}
+
 	});
 
 	return Router;
