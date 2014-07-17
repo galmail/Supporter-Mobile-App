8e1c99d20d1df3ec9a2ccbc3bdde2fb8f44813a6
Laura Carvajal <laura.carvajal@corp.badoo.com>
2014-07-17 07:44:11 +0100

    template compilation

--- js/routers/router.js
+++ js/routers/router.js
@@ -5,15 +5,23 @@ define([
 ], function ($, Backbone) {
 	'use strict';
 
-	alert('router');
+	var mainContainer = $('.main-container');
 
 	var Router = Backbone.Router.extend({
 		routes: {
-			'home': 'home',		//http://localhost/#home
-			'login': 'login'	//http://localhost/#login
+			'home': 'home', //http://localhost/#home
+			'login': 'login' //http://localhost/#login
 		},
 		home: function (param) {
-			alert('home');
+			var templateSrc = 'hello: <%= name %>';
+			var data = {
+				name: 'moe'
+			};
+
+			var compiled = _.template(templateSrc, data);
+
+			mainContainer.html(compiled);
+
 		},
 		login: function (param) {
 			alert('login');
