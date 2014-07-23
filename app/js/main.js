4503b20218722428b383b774631d13d926c08be0
Laura Carvajal <laura.carvajal@corp.badoo.com>
2014-07-23 21:09:16 +0100

    off canvas

--- app/js/main.js
+++ app/js/main.js
@@ -17,12 +17,9 @@ require.config({
 			deps: ['backbone'],
 			exports: 'Store'
 		},
-		foundation: {
-			deps: [
-				'jquery'
-			],
-			exports: 'Foundation'
-		}
+
+    	"foundation": ['jquery'],
+    	"foundation.offcanvas": ['foundation']
 	},
 	paths: {
 		jquery: 'libs/jquery/jquery',
@@ -31,15 +28,28 @@ require.config({
 		backboneLocalstorage: 'libs/backbone.localStorage/backbone.localStorage',
 		text: 'libs/requirejs-text/text',
 		modernizr: 'libs/modernizr/modernizr',
-		foundation: 'libs/foundation/foundation.min'
+		foundation: 'libs/foundation/foundation',
+    	'foundation.offcanvas': 'libs/foundation/foundation.offcanvas',
 	}
 });
 
 require([
 	'backbone',
-	'routers/router'
+	'routers/router',
+    'foundation',
+    'foundation.offcanvas'
 ], function (Backbone, Router) {
 
+	$(document).foundation({
+	  offcanvas : {
+	    open_method: 'move', // Sets method in which offcanvas opens, can also be 'overlap'
+	    close_on_click : true
+	  }
+	});
+
+
+
+    console.info('foundation', $(document).foundation, Foundation);
 	var router = Router.getInstance();
 	Backbone.history.start();
 	//router.navigate('menuLogClear', {trigger: true});
--- app/js/views/About.js
+++ app/js/views/About.js
@@ -3,8 +3,9 @@ define([
     'jquery',
     'underscore',
     'backbone',
-    'text!templates/About.html'
-], function ($, _, Backbone, templateSrc) {
+    'text!templates/About.html',
+    'text!templates/Navbar.html'
+], function ($, _, Backbone, templateSrc, navbarSrc) {
     'use strict';
 
     var View = Backbone.View.extend({
@@ -13,17 +14,18 @@ define([
 
         template: _.template(templateSrc),
 
-        events: {
-        },
+        events: {},
 
         initialize: function () {
             this.body = this.$el.parents('body');
             this.render();
+
         },
 
         render: function () {
             this.body.addClass('body-not-logged');
             this.$el.html(_.template(templateSrc));
+            $('body').prepend(_.template(navbarSrc));
         }
     });
 
