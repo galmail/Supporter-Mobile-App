f7198b9cef39ee1dc418916284956af391beec2f
Gal Dubitski <gal@tocarta.es>
2014-08-10 17:21:10 +0100

    eventByCountryDetail screen

--- app/js/views/EventByCountryDetail.js
+++ app/js/views/EventByCountryDetail.js
@@ -10,7 +10,25 @@ define([
     'use strict';
 
     var View = LoggedView.extend({
-        template: _.template(templateSrc)
+        template: _.template(templateSrc),
+        events:{
+        	'click .accordion dt': 'toggleEventDetail',
+        	'click .event .bet-type': 'betTypeClicked'
+        },
+        toggleEventDetail: function(el){
+        	el.stopPropagation();
+        	var target = $(el.currentTarget);
+        	target.find('.bet-type').toggle();
+        	target.find('.teams').toggleClass('small-6').toggleClass('small-12');
+        	target.find('.teams').toggleClass('team-title');
+        	target.next().slideToggle();
+		    return false;
+        },
+        betTypeClicked: function(el){
+        	el.stopPropagation();
+        	window.location.href = '#betOpenProcess';
+        	return false;
+        }
     });
 
     return View;
