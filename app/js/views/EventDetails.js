02ba349629afffe17d75d20644df9facf32731f7
Laura Carvajal <laura.carvajal@corp.badoo.com>
2014-08-10 19:22:54 +0100

    wip

--- app/js/views/EventDetails.js
+++ app/js/views/EventDetails.js
@@ -10,7 +10,17 @@ define([
     'use strict';
 
     var View = LoggedView.extend({
-        template: _.template(templateSrc)
+        template: _.template(templateSrc),
+        events: {
+            'click .js-accordion-header':   'toggleAccordion'
+        },
+
+        toggleAccordion: function (e) {
+            var currentTarget = $(e.currentTarget);
+            var target = $(e.target);
+            e.stopPropagation();
+            currentTarget.closest('.js-accordion-block').find('.js-content').slideToggle();
+        }
     });
 
     return View;
