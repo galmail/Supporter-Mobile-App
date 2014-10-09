19bf6ca57822272dc2e0893a80666fcd9722a34c
Gal Dubitski <gal@tocarta.es>
2014-10-09 20:56:01 +0100

    adding attributes for association

--- app/js/models/association.js
+++ app/js/models/association.js
@@ -4,7 +4,12 @@ define([
 ], function(_, Backbone){
 	var Association = Backbone.Model.extend({
 		defaults: {
-			
+			"id": null,
+	      	"name": null,
+	      	"sport": null,
+	      	"logo": "default_club.png",
+	      	"csv_id": null,
+	      	"borough": null
 		},
 		initialize: function(){
 	        console.log("New Association Created.");
