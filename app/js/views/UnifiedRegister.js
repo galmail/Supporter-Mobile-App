c51888dacc97c422c6a6b520f2bf0e820fc02dd2
Gal Dubitski <gal@tocarta.es>
2014-11-13 14:10:01 +0000

    Edit Unified Info Screen

--- app/js/views/UnifiedRegister.js
+++ app/js/views/UnifiedRegister.js
@@ -5,14 +5,14 @@ define([
     'backbone',
     'text!templates/UnifiedRegister.html',
     'views/global/UnloggedView',
-    'models/user'
-], function ($, _, Backbone, templateSrc, UnloggedView, User) {
+    'models/user',
+    'utils'
+], function ($, _, Backbone, templateSrc, UnloggedView, User, Utils) {
     'use strict';
 
     var View = UnloggedView.extend({
         template: _.template(templateSrc),
         element: '.unified-register',
-        //unifiedRegStep: 2,
        	
        	pinlookup: function(pin){
        		var self = this;
@@ -39,7 +39,26 @@ define([
         
         onRender: function(){
             var self = this;
-            // bind lookup pin event
+            if(User.LoggedUser){
+        		$('#createBtn').hide();
+        		$('#updateBtn').show();
+        		$('#cancelBtn').attr('href','#userSettings');
+        		if(User.LoggedUser.get('properties').pin){
+        			$('#pin').prop('disabled', true);
+        			self.pinlookup(User.LoggedUser.get('properties').pin);
+        		}
+        		else {
+        			$('#pin').prop('disabled', false);
+        		}
+        	}
+        	else {
+        		$('#createBtn').show();
+        		$('#updateBtn').hide();
+        		$('#cancelBtn').attr('href','#menuLogClear');
+        		$('#pin').prop('disabled', false);
+        	}
+        	
+        	// bind lookup pin event
 			$('#pin').bind({
 				keyup: function(e){
 					var charCode = (typeof e.which === "number") ? e.which : e.keyCode;
@@ -51,40 +70,57 @@ define([
 					self.pinlookup($(this).val());
 				}
 			});
-			// bind create button event
-			$('#createButton').on('click',function(){
-				// get form field values
-				var user = new User();
-				var props = {};
-				$.each(user.attributes.properties,function(key,value){
-					if(key=='birthdate'){
-						props.birthdate = {
-							year: self.$el.find('#year').val(),
-							month: self.$el.find('#month').val(),
-							day: self.$el.find('#day').val()
-						};
-					}
-					else {
-						props[key] = self.$el.find('#'+key).val();
-					}
-				});
-				user.set('properties',props);
-				user.update(function(success){
-					if(success){
-						window.location.href = "#accountsActivation";
-					}
-					else {
-						var resp = JSON.parse(response.responseText);
-        				Utils.alert(resp.message,null,'Error','Ok');
-					}
-				});
-				return false;
-			});
 			
+			$('#createBtn').on('click',function(){ self.createAccount(); });
+			$('#updateBtn').on('click',function(){ self.updateAccount(); });
 			
             return this;
-        }
+        },
         
+        updateUserInfo: function(callback){
+        	var self = this;
+        	// get form field values
+			var user = new User();
+			var props = {};
+			$.each(user.attributes.properties,function(key,value){
+				if(key=='birthdate'){
+					props.birthdate = {
+						year: self.$el.find('#year').val(),
+						month: self.$el.find('#month').val(),
+						day: self.$el.find('#day').val()
+					};
+				}
+				else {
+					props[key] = self.$el.find('#'+key).val();
+				}
+			});
+			user.set('properties',props);
+			user.update(function(success){
+				if(success){
+					callback();
+				}
+				else {
+					var resp = JSON.parse(response.responseText);
+    				Utils.alert(resp.message,null,'Error','Ok');
+				}
+			});
+        },
+        
+        createAccount: function(){
+        	this.updateUserInfo(function(){
+        		window.location.href = "#accountsActivation";
+        	});
+        	return false;
+        },
+        
+        updateAccount: function(){
+        	this.updateUserInfo(function(){
+        		Utils.alert('User Info was updated!',function(){
+        			window.location.href = "#userSettings";
+        		},'Success','Ok');
+        	});
+        	return false;
+        }
         
         
     });
