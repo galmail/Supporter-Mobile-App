76355011c83ee87b0b1042de11a830efefdb928a
Adrian Lozano <adrian@supporter.com>
2014-11-15 01:57:33 +0100

    Display TOS in an iframe

--- app/js/views/ClubDisclaimer.js
+++ app/js/views/ClubDisclaimer.js
@@ -10,7 +10,17 @@ define([
 
     var View = UnloggedView.extend({
         template: _.template(templateSrc),
-        element: '.club-disclaimer'
+        element: '.club-disclaimer',
+
+        onRender: function () {
+            var $iframe = $('#iframe-terms-and-conditions');
+            $iframe.width($iframe.parent().width());
+            $iframe.height($('#container').height()-$iframe.parent().height());
+            
+            // bind back button event
+            //$('.back-icon').on('click', this.goBack);
+            //$('#joinOperatorBtn').on('click', this.joinOperator);
+        }
     });
 
     return View;
