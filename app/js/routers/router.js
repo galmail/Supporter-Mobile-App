8feee3cb5497ecddd4bda6206aa6f29b3bdedc5c
Gal Dubitski <gal@tocarta.es>
2015-02-27 14:00:37 +0000

    OperatorsList: Back button must not be shown unless in Registration mode.

--- app/js/routers/router.js
+++ app/js/routers/router.js
@@ -99,6 +99,8 @@ define([
 
 			//Logged zone
 			'operatorsList': 				'operatorsList',
+			'operatorsList/:backbutton': 	'operatorsList',
+			
 			'userFavorites': 				'userFavorites',
 			'changeClubSelection/:club': 	'changeClubSelection',
 			'changeClub': 					'changeClub',
@@ -187,7 +189,8 @@ define([
 		},
 
 		//LOGGED ZONE
-		operatorsList: function () {
+		operatorsList: function (backbutton) {
+			window.showBackButton = backbutton;
 			new OperatorsListView();
 		},
 		userFavorites: function () {
