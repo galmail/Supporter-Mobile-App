fbbfbb4e80461fbc80a6865c6a56935a4c31400d
Gal Dubitski <gal@tocarta.es>
2015-02-27 13:10:01 +0000

    AccountsActivation: Toggle button shouldn't reset after viewing tos

--- app/js/views/AccountsActivation.js
+++ app/js/views/AccountsActivation.js
@@ -24,12 +24,18 @@ define([
         
         onInit: function(callback){
         	var self = this;
-        	// get available operators
-        	self.collection.getAvailable(function(availableOperators){
-        		console.log('loaded operators accountsActivation');
-        		Operators.ActivatedOperators = availableOperators;
+        	if(self.collection.size()==0){
+        		// get available operators
+	        	self.collection.getAvailable(function(availableOperators){
+	        		console.log('loaded operators accountsActivation');
+	        		self.collection.each(function(op){ op.set('activate','checked'); });
+	        		Operators.ActivatedOperators = availableOperators;
+	        		callback();
+	        	});
+        	}
+        	else {
         		callback();
-        	});
+        	}
         },
         
         onRender: function(callback) {
@@ -42,6 +48,16 @@ define([
 	        	Operators.SelectedOperator = self.collection.get(this.parentElement.parentElement.id).attributes;
 	        	Operators.SelectedOperator.showCreateAccountBtn = '';
 	        	window.location.href = "#operatorTerms";
+            });
+            
+            self.$el.find('input[type=checkbox]').on('change', function(){
+            	var op = self.collection.get(this.dataset.id);
+            	if(this.checked){
+            		op.set('activate','checked');
+            	}
+            	else {
+            		op.set('activate','');
+            	}
             });
             
             // bind activate accounts event
