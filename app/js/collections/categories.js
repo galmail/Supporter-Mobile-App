1156fe16b867c8de46de0bf11ee6aaa71dee5d46
Adrian Lozano <adrian@supporter.com>
2015-02-28 17:27:22 +0100

    parse error response for console.log

--- app/js/collections/categories.js
+++ app/js/collections/categories.js
@@ -8,15 +8,15 @@ define([
 ], function (_, Backbone, Category, Utils, BaseCollection, I18n) {
     var Categories = BaseCollection.extend({
         model: Category,
-        
+
         parse: function(response){
         	return response.categories;
         },
-        
+
         load: function(callback){
         	//if(BaseModel.MenuLinks.length>0) return callback(BaseModel.MenuLinks);
 			var self = this;
-			var domain = window.staticCDN || ''; 
+			var domain = window.staticCDN || '';
 			this.url = domain + '/lang/'+ I18n.locale +'/links.json?nocache='+Math.random();
         	this.$fetch({
         		success: function(collection, response, options){
@@ -24,16 +24,16 @@ define([
             		callback(collection);
             	},
             	error: function(collection, response, options){
-            		console.log('Error: ' + response);
+            		console.log('Error: ' + JSON.parse(response));
             	}
         	});
         },
-        
+
         selectByName: function(name){
         	Categories.selectedCategory = this.where({title: name})[0];
         	Categories.selectedCategory.setLinks();
         }
-        
+
     },
     // static properties
     {
